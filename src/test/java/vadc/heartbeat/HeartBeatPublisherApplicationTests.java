a0681c3d1847395b89840b04e94df43575dbd465
vadymc <vadym.chekrii@transferwise.com>
2018-12-04 07:58:54 +0200

    initial commit

---
+++ src/test/java/vadc/heartbeat/HeartBeatPublisherApplicationTests.java
@@ -0,0 +1,16 @@
+package vadc.heartbeat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HeartBeatPublisherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
